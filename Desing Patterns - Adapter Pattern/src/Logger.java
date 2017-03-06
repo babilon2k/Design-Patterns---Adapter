19ab6b9d1f4323cda233e2a952a8c3f10a873383
Mieszko Czerniak <m.czerniak@gmail.com>
2017-03-06 17:31:06 +0000

    first commit

---
+++ Desing Patterns - Adapter Pattern/src/Logger.java
@@ -0,0 +1,15 @@
+
+public class Logger
+{
+	public LogWriter logWriter;
+
+	public Logger(LogWriter logWriter)
+	{
+		this.logWriter = logWriter;
+	}
+	
+	public void write(String text)
+	{
+		logWriter.out(text);
+	}
+}
